fd0fea0fa5b1227ca4bc3cdc84ea39fdec120424
Kurkov11 <kurkovsw@icloud.com>
2023-12-22 16:14:47 +0100

    Add invalid messages to onclick function

--- index.js
+++ index.js
@@ -8,28 +8,49 @@ document.getElementById('btn').onclick = function(){
         document.getElementById('day-input').style.border = '1px solid red';
         document.getElementById('day-text').style.color = 'red';
         document.getElementById('empty-msg-day').style.display = 'block';
+        document.getElementById('invalid-msg-day').style.display = 'none';
+    }else if(dayValue <= 0 || dayValue > 31){
+        document.getElementById('day-input').style.border = '1px solid red';
+        document.getElementById('day-text').style.color = 'red';
+        document.getElementById('invalid-msg-day').style.display = 'block';
+        document.getElementById('empty-msg-day').style.display = 'none';
     }else{
         document.getElementById('day-input').style.border = '1px solid hsl(0, 0%, 86%)';
         document.getElementById('day-text').style.color = 'hsl(0, 1%, 44%)';
         document.getElementById('empty-msg-day').style.display = 'none';
+        document.getElementById('invalid-msg-day').style.display = 'none';
     }
     if(monthValue == ""){
         document.getElementById('month-input').style.border = '1px solid red';
         document.getElementById('month-text').style.color = 'red';
         document.getElementById('empty-msg-month').style.display = 'block';
+        document.getElementById('invalid-msg-month').style.display = 'none';
+    }else if(monthValue <= 0 || monthValue >= 13){
+        document.getElementById('month-input').style.border = '1px solid red';
+        document.getElementById('month-text').style.color = 'red';
+        document.getElementById('empty-msg-month').style.display = 'none';
+        document.getElementById('invalid-msg-month').style.display = 'block';
     }else{
         document.getElementById('month-input').style.border = '1px solid hsl(0, 0%, 86%)';
         document.getElementById('month-text').style.color = 'hsl(0, 1%, 44%)';
         document.getElementById('empty-msg-month').style.display = 'none';
+        document.getElementById('invalid-msg-month').style.display = 'none';
     }
     if(yearValue == ""){
         document.getElementById('year-input').style.border = '1px solid red';
         document.getElementById('year-text').style.color = 'red';
         document.getElementById('empty-msg-year').style.display = 'block';
+        document.getElementById('invalid-msg-year').style.display = 'none';
+    }else if(yearValue > 2023){
+        document.getElementById('year-input').style.border = '1px solid red';
+        document.getElementById('year-text').style.color = 'red';
+        document.getElementById('invalid-msg-year').style.display = 'block';
+        document.getElementById('empty-msg-year').style.display = 'none';
     }else{
         document.getElementById('year-input').style.border = '1px solid hsl(0, 0%, 86%)';
         document.getElementById('year-text').style.color = 'hsl(0, 1%, 44%)';
         document.getElementById('empty-msg-year').style.display = 'none';
+        document.getElementById('invalid-msg-year').style.display = 'none';
     }
 
     
