f7525aecbde49a42aa91330d3fb54635a27bcf24
Kurkov11 <kurkovsw@icloud.com>
2023-12-22 14:41:02 +0100

    Hide empty-msg and show it when field is empty

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ document.getElementById('btn').onclick = function(){
     if(document.getElementById('day-input').value == ""){
         document.getElementById('day-input').style.border = '1px solid red';
         document.getElementById('day-text').style.color = 'red';
+        document.getElementById('empty-msg').style.display = 'block';
     }
     if(document.getElementById('month-input').value == ""){
         document.getElementById('month-input').style.border = '1px solid red';
         document.getElementById('month-text').style.color = 'red';
+        document.getElementById('empty-msg').style.display = 'block';
     }
     if(document.getElementById('year-input').value == ""){
         document.getElementById('year-input').style.border = '1px solid red';
         document.getElementById('year-text').style.color = 'red';
+        document.getElementById('empty-msg').style.display = 'block';
     }
 }
